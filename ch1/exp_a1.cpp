2ff86a27ed057f455bfaa8776262dd3a84180e63
Hu Wenchao <zjuhwc@gmail.com>
2016-05-26 13:12:48 +0800

    update

---
+++ ch1/exp_a1.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+  // freopen("exp_a1.in", "r", stdin);
+  int a, b;
+  scanf("%d%d", &a, &b);
+  printf("%d\n", a+b);
+  return 0;
+}
---
+++ ch1/playground.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char const *argv[])
+{
+  // printf("%f\n", 1.0/0.0);
+  // printf("%f\n", 0.0/0.0);
+  // printf("%f\n", 1.0/0);
+  // printf("%f\n", 0/0.0);
+  // printf("%d\n", 0/0);
+  printf("%%d\n");
+  return 0;
+}
